c81c559cae3162cdf899170f788d81b22d75f4a2
Anderson Fachini <anderson.fachini@ambevtech.com.br>
2021-06-21 08:04:45 -0300

    Finished app implementation

--- Pedrapepeloutesoura/app/src/main/java/com/example/pedrapepeloutesoura/MainActivity.java
+++ Pedrapepeloutesoura/app/src/main/java/com/example/pedrapepeloutesoura/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void play(View view) {
+    public void jogar(View view) {
+        switch (view.getId()) {
+            case (R.id.btnOpcaoPedra):
+                computarJogada(OpcaoJogo.PEDRA);
+                break;
+            case (R.id.btnOpcaoPapel):
+                computarJogada(OpcaoJogo.PAPEL);
+                break;
+            case (R.id.btnOpcaoTesoura):
+                computarJogada(OpcaoJogo.TESOURA);
+                break;
+        }
+    }
 
+    private OpcaoJogo getOpcaoApp() {
+        int idx = new Random().nextInt(3);
+        return OpcaoJogo.values()[idx];
+    }
+
+    private void computarJogada(OpcaoJogo opcaoUsuario) {
+        OpcaoJogo opcaoApp = getOpcaoApp();
+
+        String resultado = "";
+
+        if (opcaoApp == opcaoUsuario) {
+            resultado = "Empatou!";
+        } else {
+            resultado = "Você perdeu :(";
+        }
+
+        if ((opcaoUsuario == OpcaoJogo.PEDRA && opcaoApp == OpcaoJogo.TESOURA) || //
+                (opcaoUsuario == OpcaoJogo.PAPEL && opcaoApp == OpcaoJogo.PEDRA) || //
+                (opcaoUsuario == OpcaoJogo.TESOURA && opcaoApp == OpcaoJogo.PAPEL) ) {
+            resultado = "Você ganhou :)";
+        }
+
+        TextView textResultado = findViewById(R.id.textResultado);
+        textResultado.setText(resultado);
+
+        ImageView img = findViewById(R.id.imageOpcaoApp);
+        switch (opcaoApp) {
+            case PEDRA: img.setImageResource(R.drawable.pedra); break;
+            case PAPEL: img.setImageResource(R.drawable.papel); break;
+            case TESOURA: img.setImageResource(R.drawable.tesoura); break;
+        }
     }
 
 }
---
+++ Pedrapepeloutesoura/app/src/main/java/com/example/pedrapepeloutesoura/OpcaoJogo.java
@@ -0,0 +1,5 @@
+package com.example.pedrapepeloutesoura;
+
+public enum OpcaoJogo {
+    PEDRA, PAPEL, TESOURA;
+}
